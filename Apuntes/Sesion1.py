19bf51994a5d50e30d63104266974a3871cea3af
JoseRopero <josemanuelropero@hotmail.com>
2022-11-16 00:58:21 +0100

    Update List

--- Apuntes/Sesion1.py
+++ Apuntes/Sesion1.py
@@ -25,6 +25,7 @@ print(f"Cuantas veces aparece 'ho': {cadena.count('ho')}")  # El método count()
 print(f"La cadena termina en on: {cadena.endswith('on')}")
 print(f"Reemplazamos la primera palabra: {cadena.replace('hola', 'Guay')}")  # Reemplaza la palabra.
 print(f"Buscamos la palabra 'Vamos': {cadena.find('Vamos')}")  # Busca una palabra.
+print(f"Primera aparición en la cadena de P: {cadena.index('P')}")
 multiString = '''Hola, vamos con Python y las,
 multilínea.
 Otra manera de escribir texto en Python.
@@ -62,15 +63,35 @@ n1 = abs(-9)  # Valor absoluto
 print(f"El valor absoluto: {n1}")
 print()
 
-# 4. Trabajamos con las listas.
+# 4. Trabajamos con las listas. Tipo de colección ordenada y modificable. Admite duplicados.
 print("LISTAS:")
 vacio = []  # Se puede crear una lista vacía.
+vacio2 = list()
 lista1 = ['a', 'b', 'c', 'd', 'e', 'f', 'x', 'm']  # De esta manera inicializamos una lista.
+lista4 = ['a', 5, "Hola como estas", {'Country': 'España', 'City': 'Málaga'}]  # Pueden tener distintos datos.
+# Ejemplos desempaquetar elementos de una lista
+fruits = ['banana', 'orange', 'mango', 'lemon', 'lime', 'apple']
+first_fruit, second_fruit, third_fruit, *rest = fruits
+print(first_fruit)
+print(second_fruit)
+print(third_fruit)
+print(rest)
+first, second, third, *rest, tenth = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]  # Desempaquetando
+print(f"{first} {second} {third} {rest} {tenth}")
+countries = ['Germany', 'France', 'Belgium', 'Sweden', 'Denmark', 'Finland', 'Norway', 'Iceland', 'Estonia']
+gr, fr, bg, sw, *scandic, es = countries
+print(gr)
+print(fr)
+print(bg)
+print(sw)
+print(scandic)
+print(es)
+
 lista1.remove('c')  # Eliminamos un elemento de la lista.
 lista2 = ['g', 'h', 'i', 'j', 'k', 'l']
 lista1.append(lista2)  # Añadimos la lista2 a la lista1 (lista anidada).
 print(f"Lista anidada: {lista1}")
-lista1.insert(2, 'c')  # Para introducir un valor en el índice indicado.
+lista1.insert(2, 'c')  # Para introducir un valor en el índice indicado. No sustituye.
 print(f"Añadimos un elemento a la lista en el indice 2: {lista1}")
 lista1.extend(['o', 'p', 'q'])  # Añade una lista de elementos a la original.
 print(f"Extendemos la lista: {lista1}")
@@ -79,11 +100,16 @@ print(f"Imprimo por rangos: {lista1[0:3]}")  # Imprime por rangos. El segundo pa
 print(lista1[:3])  # Es lo mismo que la línea anterior.
 print(lista1[2:])  # Imprime desde el segundo elemento hasta el final.
 print(f"Está la letra 'd' en  la lista? {'d' in lista1}")  # Devuelve true o false si el elemento esta en la lista.
-print(f"Cuantas veces aparece la letra 'x': {lista1.count('x')}")  # Count nos muestra cuantas veces aparece un elemento en la lista.
-lista1.pop()  # Elimina el último elemento de la lista.
+print(f"Cuantas veces aparece la letra 'x': {lista1.count('x')}")
+lista1.pop()  # Elimina el último elemento de la lista si no indicamos el índice.
 print(lista1)
+# del lista1[2] -> Elimina por índice
+# del lista1[2:4] -> Elimina por rangos
+# del lista1 → Elimina la lista
 lista3 = lista1 * 2  # Duplicamos la lista.
-print(lista3)
+print(f"Duplicamos la lista: {lista3}")
+lista3.reverse()
+print(f"Reverse: {lista3}")
 lista3.clear()  # Vacía la lista.
 lista3 = [5, 9, 10, -4, 3, 59]
 print(lista3)
@@ -93,7 +119,7 @@ lista3.sort(reverse=True)
 print(f"Revertimos la lista: {lista3}")
 print()
 
-# 5. Tupla
+# 5. Tupla. Tipo de colección ordenada e inmutable. Permite duplicados.
 print("TUPLA:")
 tupla = ('a', 'b', "Hola", 'c', 5, 6, 5.98, [1, 2, 3, 4], 'd', 'e')  # Inicializamos una tupla.
 print(tupla)
@@ -105,7 +131,7 @@ tupla1 = tuple(lista4)  # Pasamos la lista a una tupla.
 print(tupla1)
 print()
 
-# 6. Diccionario
+# 6. Diccionario. Tipo de colección desordenada, modificable e indexada. No hay duplicados.
 print("DICCIONARIO:")
 diccionario = {'a': 1, 'b': 2, 'c': 3, 'd': 4}  # Inicializamos un diccionario.
 print(diccionario['a'])  # Imprime el valor de la clave.
@@ -115,7 +141,7 @@ diccionario.pop('d')  # Elimina la clave valor.
 del diccionario['a']  # Otra manera de eliminar.
 print()
 
-# 7. Conjuntos (set)
+# 7. Conjuntos(set). Colección desordenada, no indexada e inmutable pero podemos agregar nuevos elementos. No duplicados
 print("SETS:")
 a = {1, 2, 3, 4, 5}  # Inicializamos un set (Conjuntos). No se pueden repetir los elementos.
 b = {2, 5, 9, 8, 10}
